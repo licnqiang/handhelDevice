38d3d6d61f33908257d1c25abfaa5661da802b15
luosen <lzh92618>
2019-10-23 15:06:56 +0800

    个人中心页面

--- app/src/main/java/cn/piesat/sanitation/ui/fragment/MeFragment.java
+++ app/src/main/java/cn/piesat/sanitation/ui/fragment/MeFragment.java
@@ -1,14 +1,7 @@
 package cn.piesat.sanitation.ui.fragment;
 
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 import cn.piesat.sanitation.R;
 import cn.piesat.sanitation.common.BaseFragment;
 
@@ -17,7 +10,6 @@ import cn.piesat.sanitation.common.BaseFragment;
  */
 public class MeFragment extends BaseFragment {
 
-    Unbinder unbinder;
 
     @Override
     protected int getLayoutId() {
@@ -33,15 +25,4 @@ public class MeFragment extends BaseFragment {
     }
 
 
-    @OnClick({R.id.btn_start, R.id.btn_stop})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.btn_start:
-                showLoadingDialog("加载中...",true);
-                break;
-            case R.id.btn_stop:
-                dismiss();
-                break;
-        }
-    }
 }
